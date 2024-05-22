c96c5b97d7bc8dca82d8cfb4212eca6ffb9c6796
Leonardo de Oliveira Ramos <oliveirarleo@gmail.com>
2024-05-22 20:13:41 -0300

    feat: light/dark mode and posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
+export const blog = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
   schema: z.object({
