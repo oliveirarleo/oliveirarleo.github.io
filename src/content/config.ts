97e5ed6cbcb6848625ae8a986e8a0599e7d7f78e
Leonardo de Oliveira Ramos <oliveirarleo@gmail.com>
2024-05-25 15:19:37 -0300

    feat: add enhanced images

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,15 +2,14 @@ import { defineCollection, z } from "astro:content";
 
 export const blog = defineCollection({
   type: "content",
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image(),
+    }),
 });
 
 export const collections = { tech: blog, personal: blog };
